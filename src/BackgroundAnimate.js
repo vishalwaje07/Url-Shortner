80773670c0325c22c50037f4b6ccd32f5d86ca77
vishalwaje07 <129249020+vishalwaje07@users.noreply.github.com>
2023-07-06 10:34:26 +0530

    Initial commit

---
+++ src/BackgroundAnimate.js
@@ -0,0 +1,24 @@
+
+
+const BackgroundAnimate = () => {
+    return (
+        <ul className="background">
+            <div class="background">
+            <ul class="background">
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+</ul>
+</div>
+        </ul>
+    )
+}
+
+export default BackgroundAnimate
